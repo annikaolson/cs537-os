a01c109790c58fd934e921b12545f726ad6c6722
Annika Tru Olson <atolson3@wisc.edu>
2024-10-31 14:11:56 -0500

    update pass

--- p4/solution/proc.c
+++ p4/solution/proc.c
@@ -350,6 +350,8 @@ scheduler(void)
       if(p->state != RUNNABLE)
         continue;
 
+      p->pass += p->stride;
+
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
